aeae99c01ce4255168308b17ad83ffffea817617
AkashLM <90713041+AkashLM@users.noreply.github.com>
2021-09-14 22:26:37 +0530

    Day 1 Assignment submitted

---
+++ Day1 Assignment/day1.py
@@ -0,0 +1,30 @@
+#Question 1
+
+var1="Akash"
+
+var2="Akash"
+print(var1 is var2)
+
+var3=8.5547
+
+var4=8.
+print(var3 is var4)
+
+
+#Question 2
+
+print(1>3>4)
+
+#Question 2
+
+str='250'
+print("int('250') with base 2=", int(str,2)")
+s=str
+
+print("Value of str Function = ",s)
+
+f_string=float("5.6")
+print("Value of float function =", f_string)
+
+
+
